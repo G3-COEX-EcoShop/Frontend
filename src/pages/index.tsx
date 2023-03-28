7102580c2dacfdc4ae53f854000998e43dfc48a6
Mauricio Arias <50003142+ImCrisam@users.noreply.github.com>
2023-03-28 15:37:37 -0500

    fix meta tag social

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-      <ShopLayout title={'EcoShop'} pageDescription={'Bienvenido a nuestra tienda de electrónica, donde ofrecemos una amplia variedad de productos de tecnología de vanguardia, como teléfonos celulares, computadoras y accesorios. Encuentra todo lo que necesitas para estar conectado y actualizado con las últimas tendencias en tecnología. Ofrecemos productos de alta calidad a precios competitivos. ¡Visítanos ahora para explorar nuestra selección!'} imageFullUrl='https://drive.google.com/file/d/12oKDiIjtA29ongN-kQkjMcjC-DYLAq0a/view?usp=sharing'>
+      <ShopLayout title={'Tienda EcoShop'} pageDescription={'Bienvenido a nuestra tienda de electrónica, donde ofrecemos una amplia variedad de productos de tecnología de vanguardia'} imageFullUrl='https://lh3.googleusercontent.com/u/0/drive-viewer/AAOQEOQnX9lPLVoh3eL1WKMqg2-dZlDDsfO4H_JusGDbkNBFF7ugyWllUCV5wipPARdgpGLp8srtCotMFYWTlynZiHvVe0vA=w1920-h975'>
         <>
           <Typography variant='h1' component="h1">Tienda</Typography>
           <Typography variant='h2' component="h2" sx={{ mb: 1 }} > EcoShop</Typography>
