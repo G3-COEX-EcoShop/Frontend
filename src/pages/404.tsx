92fc077ac14dcb534d0a4d22d38ea2ad9f931598
Mauricio Arias <50003142+ImCrisam@users.noreply.github.com>
2023-03-28 18:09:36 -0500

    fix 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,4 @@
+import { Box, Typography } from '@mui/material';
 import React from 'react'
 import { ShopLayout } from '../components/layout/ShopLayout';
 
@@ -5,7 +6,16 @@ const custom404 = () => {
     return (
         <ShopLayout title={'not found'} pageDescription={''}>
 
-            <h1>custom-404</h1>
+            <Box
+                display='flex'
+                justifyContent='center'
+                alignItems='center'
+                height='calc(100vh - 200px)'
+                sx={{ flexDirection: { xs: 'column', sm: 'row' } }}
+            >
+                <Typography variant='h1' component='h1' fontSize={80} fontWeight={200}>404 |</Typography>
+                <Typography marginLeft={2}>No encontramos ninguna página aquí</Typography>
+            </Box>
         </ShopLayout>
     )
 }
