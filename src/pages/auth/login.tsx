de25b037f456568583be77bafa38e112ec2669bc
Mauricio Arias <50003142+ImCrisam@users.noreply.github.com>
2023-04-16 18:12:09 -0500

    fix

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -18,26 +18,27 @@ const login = () => {
                 alert(res.error)
             }
         }
-
-        return (
-            <BasicoLayout >
-
-                <Grid container spacing={3}>
-                    <Grid item xs={12}>
-                        <Stack direction="row" justifyContent="space-between" alignItems="baseline" sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
-                            <Typography variant="h3">Login</Typography>
-                            <Typography variant="body1" sx={{ textDecoration: 'none' }} color="primary">
-                                Don&apos;t have an account?
-                            </Typography>
-                        </Stack>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <LoginForm onSubmit={onSubmit} />
-                    </Grid>
-                </Grid>
-            </BasicoLayout>
-        )
     }
 
+    return (
+        <BasicoLayout >
 
-    export default login
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Stack direction="row" justifyContent="space-between" alignItems="baseline" sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
+                        <Typography variant="h3">Login</Typography>
+                        <Typography variant="body1" sx={{ textDecoration: 'none' }} color="primary">
+                            Don&apos;t have an account?
+                        </Typography>
+                    </Stack>
+                </Grid>
+                <Grid item xs={12}>
+                    <LoginForm onSubmit={onSubmit} />
+                </Grid>
+            </Grid>
+        </BasicoLayout>
+    )
+}
+
+
+export default login
