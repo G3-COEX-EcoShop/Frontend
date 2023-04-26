4ebc6245d1d88b1541da79a87633cd01886295c7
Mauricio Arias <50003142+ImCrisam@users.noreply.github.com>
2023-04-25 23:21:23 -0500

    erro sync component

--- src/components/dashboard/modals/ModalProduct.tsx
+++ src/components/dashboard/modals/ModalProduct.tsx
@@ -1,4 +1,4 @@
-import { Autocomplete, Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Stack, Switch, TextField, TextareaAutosize, Typography, useMediaQuery, useTheme, } from "@mui/material";
+import { Autocomplete, Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogProps, DialogTitle, FormControl, InputLabel, MenuItem, Select, Stack, Switch, TextField, TextareaAutosize, Typography, useMediaQuery, useTheme, } from "@mui/material";
 
 import { IProduct } from "@/interfaces";
 import { FormEvent, useContext, useEffect, useState } from "react";
@@ -6,6 +6,8 @@ import UploadImg from "@/components/utils/UploadImg";
 import { brandList } from "@/services/brand";
 import { crud } from "@/interfaces/utils";
 import { RoleContext } from "@/context";
+import { productsQuery } from "@/services/product";
+import { NonNullExpression } from "typescript";
 
 interface CreateModalProps {
     data: IProduct | null;
@@ -22,28 +24,65 @@ export const ModalProduct = ({
     onClose,
     onSubmit,
 }: CreateModalProps) => {
-
     const { rol } = useContext(RoleContext)
 
     const [urlImg, seturlImg] = useState("")
     const [errorUpload, seterrorUpload] = useState("")
     const theme = useTheme();
     const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+
     const [brands, setBrands] = useState<string[]>([])
 
+    const [product, setProduct] = useState<IProduct | null>(data)
+
+    const [category, setCategory] = useState("computadores")
+
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchDataBrands = async () => {
             const data = await brandList()
             setBrands(data.map((item) => (item.name)))
 
         }
-        fetchData()
+        fetchDataBrands()
     }, [])
+
+    useEffect(() => {
+        setCategory(data?.category ? data?.category : "computadores")
+        console.log({ data });
+
+
+        if (data?.id) {
+            (async () => {
+                const res = await productsQuery(data.id + "")
+                setProduct(res.data)
+                console.log({ product });
+            })
+            console.log({ product });
+
+
+        } else {
+            setProduct(data)
+            console.log({ product });
+
+        }
+
+    }, [data])
+
+    useEffect(() => {
+        setCategory(product?.Category + "")
+
+    }, [product])
+
+
+
+
 
 
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
+        console.log("submit");
+
         const target: any[] = e.target as any
         const type: crud = data ? "update" : "create"
 
@@ -67,6 +106,8 @@ export const ModalProduct = ({
             }
 
         }
+        console.log({ res });
+
         onSubmit(res, type)
         onClose();
     };
@@ -81,11 +122,12 @@ export const ModalProduct = ({
 
 
     return (
-        <Dialog open={open} fullScreen={fullScreen}
-        >
+        <form onSubmit={handleSubmit} id="formModal">
+            <Dialog open={open} fullScreen={fullScreen}
+            >
+                <DialogContent  >
 
-            <DialogContent>
-                <form onSubmit={handleSubmit}>
+
                     <Stack
                         sx={{
                             minWidth: { xs: '300px', sm: '360px', md: '400px' },
@@ -96,77 +138,269 @@ export const ModalProduct = ({
                         <TextField
                             id="name"
                             label="Nombre"
-                            defaultValue={data?.name}
+                            defaultValue={product?.name}
 
                             required
                         />
                         <Box display={"flex"} flexDirection={"column"}>
                             <Typography >Descripcion</Typography>
-                            <TextareaAutosize name="Soft" placeholder="Descripcion de la categoria" minRows={5} defaultValue={data?.description} required />
+                            <TextareaAutosize name="Soft" placeholder="Descripcion de la categoria" minRows={5} defaultValue={product?.description} required />
                         </Box>
 
                         <Box display={"flex"} alignItems={"center"}>
                             <Typography>Estado </Typography>
-                            <Typography>{data?.status} </Typography>
-                            <Switch defaultChecked={data?.status} id="status" />
+                            <Typography>{product?.status} </Typography>
+                            <Switch defaultChecked={product?.status} id="status" />
 
                         </Box>
-                        <Autocomplete
-                            id="category"
 
-                            options={categories}
-                            defaultValue={data?.category}
-                            renderInput={(params) => <TextField {...params} label="Categoria" required id="category" />}
-                        />
-                        <Autocomplete
-                            disablePortal
-                            id="brands"
-                            options={brands}
-                            defaultValue={data?.brand}
-                            renderInput={(params) => <TextField {...params} label="Marca" required id="brands" />}
-                        />
+                        <FormControl >
+                            <InputLabel id="category">categoria</InputLabel>
+                            <Select
+                                id="category"
+                                labelId="category"
+                                label="categoria"
+                                required
+                                value={category}
+                                onChange={(item) => {
+                                    setCategory(item.target.value)
+                                }}
+                            >
+                                {
+                                    categories.map((item) => (
+                                        <MenuItem key={item} value={item}>{item}</MenuItem>
+                                    ))
+                                }
+
+                            </Select>
+
+                        </FormControl>
+                        <FormControl >
+                            <InputLabel id="brands">marca</InputLabel>
+                            <Select
+                                id="brands"
+                                labelId="brands"
+                                defaultValue={product?.brand ? product?.brand : brands[0]}
+                                label="categoria"
+                                required
+                            >
+                                {
+                                    brands.map((item) => (
+                                        <MenuItem key={item} value={item}>{item}</MenuItem>
+                                    ))
+                                }
+
+                            </Select>
+                        </FormControl>
 
                         <TextField
                             id="price"
                             type="number"
                             label="Precio"
-                            defaultValue={data?.price}
+                            defaultValue={product?.price}
                             required
                         />
                         <TextField
                             id="stock"
                             type="number"
                             label="Cantidad"
-                            defaultValue={data?.stock}
+                            defaultValue={product?.stock}
                             required
                         />
                         <UploadImg
-                            imgInit={data?.img_url}
+                            imgInit={product?.img_url}
                             setUrl={seturlImg}
                             seterror={seterrorUpload}
                         ></UploadImg>
+                        {
+                            (category == "celulares") &&
+                            <>
 
+                                <TextField
+                                    id="operating_system"
+                                    type="text"
+                                    label="Sistema opetativo"
+                                    defaultValue={product?.ProductCel?.operating_system}
+                                    required
+                                />
+                                <TextField
+                                    id="storage"
+                                    type="text"
+                                    label="Almacenamiento"
+                                    defaultValue={product?.ProductCel?.storage}
+                                    required
+                                />
+                                <TextField
+                                    id="ram"
+                                    type="text"
+                                    label="RAM"
+                                    defaultValue={product?.ProductCel?.ram}
+                                    required
+                                />
+                                <TextField
+                                    id="processor"
+                                    type="text"
+                                    label="Procesador"
+                                    defaultValue={product?.ProductCel?.processor}
+                                    required
+                                />
+                                <TextField
+                                    id="screen_size"
+                                    type="text"
+                                    label="Pantalla"
+                                    defaultValue={product?.ProductCel?.screen_size}
+                                    required
+                                />
+                                <TextField
+                                    id="resolution"
+                                    type="text"
+                                    label="Resolucion"
+                                    defaultValue={product?.ProductCel?.resolution}
+                                    required
+                                />
+                                <TextField
+                                    id="main_camera"
+                                    type="text"
+                                    label="Camara principal"
+                                    defaultValue={product?.ProductCel?.main_camera}
+                                    required
+                                />
+                                <TextField
+                                    id="front_camera"
+                                    type="text"
+                                    label="Camara frontal"
+                                    defaultValue={product?.ProductCel?.front_camera}
+                                    required
+                                />
+                                <TextField
+                                    id="battery"
+                                    type="text"
+                                    label="Bateria"
+                                    defaultValue={product?.ProductCel?.battery}
+                                    required
+                                />
+
+                            </>
+                        }
+                        {
+                            (category == "televisores") &&
+                            <>
+
+                                <TextField
+                                    id="display_technology"
+                                    type="text"
+                                    label="Tipo de pantalla"
+                                    defaultValue={product?.ProductTV?.display_technology}
+                                    required
+                                />
+                                <TextField
+                                    id="hdmi"
+                                    type="text"
+                                    label="hdmi"
+                                    defaultValue={product?.ProductTV?.hdmi}
+                                    required
+                                />
+
+                                <TextField
+                                    id="screen_size"
+                                    type="text"
+                                    label="Pantalla"
+                                    defaultValue={product?.ProductTV?.screen_size}
+                                    required
+                                />
+                                <TextField
+                                    id="resolution"
+                                    type="text"
+                                    label="Resolucion"
+                                    defaultValue={product?.ProductTV?.resolution}
+                                    required
+                                />
+
+                            </>
+                        }
+                        {
+                            (category == "computadores") &&
+                            <>
+                                <TextField
+                                    id="operating_system"
+                                    type="text"
+                                    label="Sistema opetativo"
+                                    defaultValue={product?.ProductLaptop?.operating_system}
+                                    required
+                                />
+                                <TextField
+                                    id="storage"
+                                    type="text"
+                                    label="Almacenamiento"
+                                    defaultValue={product?.ProductLaptop?.storage}
+                                    required
+                                />
+                                <TextField
+                                    id="ram"
+                                    type="text"
+                                    label="RAM"
+                                    defaultValue={product?.ProductLaptop?.ram}
+                                    required
+                                />
+                                <TextField
+                                    id="cpu_brand"
+                                    type="text"
+                                    label="Procesador"
+                                    defaultValue={product?.ProductLaptop?.cpu_brand}
+                                    required
+                                />
+                                <TextField
+                                    id="cpu_model"
+                                    type="text"
+                                    label="Procesador"
+                                    defaultValue={product?.ProductLaptop?.cpu_model}
+                                    required
+                                />
+                                <TextField
+                                    id="screen_size"
+                                    type="text"
+                                    label="Pantalla"
+                                    defaultValue={product?.ProductLaptop?.screen_size}
+                                    required
+                                />
+                                <TextField
+                                    id="resolution"
+                                    type="text"
+                                    label="Resolucion"
+                                    defaultValue={product?.ProductLaptop?.resolution}
+                                    required
+                                />
+
+                                <TextField
+                                    id="graphics_coprocessor"
+                                    type="text"
+                                    label="Graficos"
+                                    defaultValue={product?.ProductLaptop?.graphics_coprocessor}
+                                    required
+                                />
+
+                            </>
+                        }
 
                     </Stack>
-                    <DialogActions sx={{ p: '1.25rem', marginTop: "2rem" }} >
-                        <Box marginRight={"auto"}>
 
-                            {
-                                (rol.productPermission?.can_delete && data) && <Button color="primary" variant="text" onClick={handleDelete}> Eliminar</Button>
-                            }
-                        </Box>
+                </DialogContent>
+                <DialogActions sx={{ p: '1.25rem', marginTop: "2rem" }} >
+                    <Box marginRight={"auto"}>
+
                         {
-                            data ? (
-                                <Button color="info" variant="contained" type="submit"> Editar</Button>
-                            ) : (
-                                <Button color="info" variant="contained" type="submit"> Crear</Button>
-                            )
+                            (rol.productPermission?.can_delete && product) && <Button color="primary" variant="text" onClick={handleDelete}> Eliminar</Button>
                         }
-                        <Button onClick={onClose}>Cancel</Button>
+                    </Box>
+                    <Button color="info" variant="contained" type="submit" form="formModal">
+                        {
+                            product ? ("Editar") : ("Crear")
+                        }
+                    </Button>
+                    <Button onClick={onClose}>Cancel</Button>
 
-                    </DialogActions>
-                </form>
-            </DialogContent>
-        </Dialog >
+                </DialogActions>
+            </Dialog >
+        </form>
     );
 };
--- src/interfaces/products.ts
+++ src/interfaces/products.ts
@@ -10,6 +10,12 @@ export interface IProduct {
   status?: boolean;
   createdAt?: string;
   updatedAt?: string;
+  Brand?: IBrand;
+  Category?: ICategory;
+  Reviews?: IReview[];
+  ProductCel?: IProductCel;
+  ProductLaptop?: IProductLaptop;
+  ProductTV?: IProductTv;
 }
 export interface IProductCart {
   id: number;
@@ -38,4 +44,56 @@ export interface IBrand {
   status: boolean;
   createdAt?: any;
   updatedAt?: any;
+}
+
+export interface IReview {
+  id: number;
+  description: string;
+  point: number;
+  id_product: number;
+  id_user: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IProductCel {
+  id: number;
+  id_product: number;
+  operating_system: string;
+  storage: string;
+  ram: string;
+  processor: string;
+  screen_size: string;
+  resolution: string;
+  main_camera: string;
+  front_camera: string;
+  battery: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IProductTv {
+  id: number;
+  id_product: number;
+  display_technology: string;
+  resolution: string;
+  screen_size: string;
+  hdmi: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IProductLaptop {
+  id: number;
+  id_product: number;
+  cpu_brand: string;
+  cpu_model: string;
+  graphics_coprocessor: string;
+  storage: string;
+  ram: string;
+  operating_system: string;
+  screen_size: string;
+  resolution: string;
+  createdAt: string;
+  updatedAt: string;
 }
